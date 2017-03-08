c7f2b82a0e3ad4fdf59be25e733123f72025fb9d
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-03-08 16:56:58 -0300

    Incluindo modal resumo série

--- js/script.js
+++ js/script.js
@@ -6,3 +6,10 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
